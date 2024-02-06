0d374f8b3db4a16018630682a646baeb07a11b9e
takapdayon <business.takap@gmail.com>
2024-02-06 22:53:18 +0900

    add: no index

--- src/app/music/layout.tsx
+++ src/app/music/layout.tsx
@@ -4,6 +4,9 @@ export const metadata = {
   title: 'yoshika music',
   description:
     'yoshika music collected by youtube and twitch, if you want to upload. please check it',
+  robots: {
+    index: false, // noindexの設定
+  },
 };
 
 const Layout = ({ children }: { children: ReactNode }) => <>{children}</>;

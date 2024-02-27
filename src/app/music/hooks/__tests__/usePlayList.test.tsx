38cddb06237f1da3a2165a1341608525b94b8053
takapdayon <business.takap@gmail.com>
2024-02-28 00:26:26 +0900

    test: test修正

--- src/app/music/hooks/__tests__/usePlayList.test.tsx
+++ src/app/music/hooks/__tests__/usePlayList.test.tsx
@@ -1,8 +1,13 @@
 import { act, renderHook } from '@testing-library/react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { usePlayList } from '../usePlayList';
 
+vi.mock('uuid');
+
 describe('usePlayListのテスト', () => {
+  (uuidv4 as jest.Mock).mockReturnValue('uuid-test');
+
   const data1 = {
     id: '001',
     youtubeUrl: '9QyhCXilmd0',
@@ -10,7 +15,7 @@ describe('usePlayListのテスト', () => {
     artist: 'YOASOBI',
     playStartTime: 4230,
     playEndTime: 4441,
-    formatPlayTime: 3230,
+    formattedPlayTime: '03:30',
     playTime: 3441 - 4230,
     youtubeTitle:
       '【#歌枠】元気に楽しく歌います！🍠Singing Stream🍠Karaoke【Vtuber】',
@@ -22,7 +27,7 @@ describe('usePlayListのテスト', () => {
     artist: 'YOASOBI',
     playStartTime: 3230,
     playEndTime: 3441,
-    formatPlayTime: 3230,
+    formattedPlayTime: '03:30',
     playTime: 3441 - 3230,
     youtubeTitle:
       '【#歌枠】元気に楽しく歌います！🍠Singing Stream🍠Karaoke【Vtuber】',
@@ -39,27 +44,43 @@ describe('usePlayListのテスト', () => {
   test('addPlayListで値が正しくセットされること', () => {
     const { result } = renderHook(() => usePlayList());
     act(() => result.current.addPlayList(data1));
-    expect(result.current.playList).toStrictEqual([data1]);
+    expect(result.current.playList).toStrictEqual([
+      { ...data1, playListId: 'uuid-test' },
+    ]);
     act(() => result.current.addPlayList(data2));
-    expect(result.current.playList).toStrictEqual([data1, data2]);
+    expect(result.current.playList).toStrictEqual([
+      { ...data1, playListId: 'uuid-test' },
+      { ...data2, playListId: 'uuid-test' },
+    ]);
   });
 
   test('delPlayListで値が正しく削除されること', () => {
     const { result } = renderHook(() => usePlayList());
     act(() => result.current.addPlayList(data1));
     act(() => result.current.addPlayList(data2));
-    expect(result.current.playList).toStrictEqual([data1, data2]);
+    expect(result.current.playList).toStrictEqual([
+      { ...data1, playListId: 'uuid-test' },
+      { ...data2, playListId: 'uuid-test' },
+    ]);
     act(() => result.current.delPlayList(1));
-    expect(result.current.playList).toStrictEqual([data1]);
+    expect(result.current.playList).toStrictEqual([
+      { ...data1, playListId: 'uuid-test' },
+    ]);
   });
 
   test('selectPlayMusicで値が正しくセットされること', () => {
     const { result } = renderHook(() => usePlayList());
     act(() => result.current.addPlayList(data1));
     act(() => result.current.addPlayList(data2));
-    expect(result.current.playingMusic).toStrictEqual(data1);
+    expect(result.current.playingMusic).toStrictEqual({
+      ...data1,
+      playListId: 'uuid-test',
+    });
     act(() => result.current.selectPlayMusic(1));
-    expect(result.current.playingMusic).toStrictEqual(data2);
+    expect(result.current.playingMusic).toStrictEqual({
+      ...data2,
+      playListId: 'uuid-test',
+    });
   });
 
   test('onEndPlayingMusicで値が正しくセットされること', () => {
@@ -68,8 +89,12 @@ describe('usePlayListのテスト', () => {
     act(() => result.current.addPlayList(data2));
     act(() => result.current.selectPlayMusic(1));
     act(() => result.current.onEndPlayingMusic());
-    expect(result.current.playList).toStrictEqual([data1]);
-    expect(result.current.playedPlayList).toStrictEqual([data2]);
+    expect(result.current.playList).toStrictEqual([
+      { ...data1, playListId: 'uuid-test' },
+    ]);
+    expect(result.current.playedPlayList).toStrictEqual([
+      { ...data2, playListId: 'uuid-test' },
+    ]);
     act(() => result.current.selectPlayMusic(0));
   });
 });
